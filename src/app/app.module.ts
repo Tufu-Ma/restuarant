c35d8d1e586257977537ee77599b6efb474b0f59
TufuXD <surachai.ma@ku.th>
2024-09-07 21:44:49 +0700

    sent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    MenuComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
